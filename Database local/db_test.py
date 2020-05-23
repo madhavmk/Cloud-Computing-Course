ca125850f4caf7a50335be87735a6a202eb735d3
madhavmk <madhavkashyap99@gmail.com>
2020-05-23 11:56:00 +0530

    files removed

--- Database local/db_test.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-print('trying connection')
-db = psycopg2.connect("dbname='Cloud_Computing_Assignment' user='postgres' host='52.73.30.120' password='Iusepostgres@321'")
-print('success')
-#Public IP
-#db = psycopg2.connect("dbname='Cloud_Computing_Assignment' user='postgres' host='52.73.30.120' password='Iusepostgres@321'")
-
-#Private IP 10 times faster ping
-#db = psycopg2.connect("dbname='Cloud_Computing_Assignment' user='postgres' host='172.31.88.235' password='Iusepostgres@321'")
--- Database local/trial.py
+++
@@ -1,4 +0,0 @@
-import requests 
-  
-temp_request = requests.get(url = "http://52.203.75.160:8080/api/v1/users") 
-data = temp_request.json())
